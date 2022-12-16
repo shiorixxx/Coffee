63f27dd15208f7f7c43f50e7cc0527f920d9086a
Shiori Koga <shiori.neoardent@gmail.com>
2022-12-15 17:37:13 -0800

    image change

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//スムーススクロール
+
+$(function () {
+  /*================= 
+    スムーススクロール 
+    ================= */
+  // ページ内のリンクをクリックした時に動作する
+  $('a[href^="#"]').click(function () {
+    // リンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット
+    let target = $(href == "#" || href == "" ? "href" : href);
+    // トップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う
+    // 600はスクロール速度で単位はミリ秒
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
+});
